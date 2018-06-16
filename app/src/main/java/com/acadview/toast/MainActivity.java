167122b643c3a147a307ffdfe8e8e9b4b3896644
Tamanna Chauhan <chauhantamanna001@gmail.com>
2018-06-16 01:51:47 -0700

    first commit

---
+++ app/src/main/java/com/acadview/toast/MainActivity.java
@@ -0,0 +1,45 @@
+package com.acadview.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Toast in onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Toast in onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Toast in onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "This is my Toast in onDestroyed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "This is my Toast in onDestoyed", Toast.LENGTH_SHORT).show();
+    }
+}
